c71f691c74e4629b44491274891fa1a84695618f
revlinaibaho <revlinaibaho@gmail.com>
2021-04-15 08:34:56 +0700

    Pertama

---
+++ app/src/main/java/org/tensorflow/lite/examples/detection/customview/Speech.java
@@ -0,0 +1,13 @@
+package org.tensorflow.lite.examples.detection.customview;
+
+import android.graphics.Bitmap;
+import android.speech.tts.TextToSpeech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.tensorflow.lite.examples.detection.tflite.Classifier;
+
+
+public class Speech extends AppCompatActivity {
+
+};
